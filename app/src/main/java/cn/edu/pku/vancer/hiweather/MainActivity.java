b59a6de142407a63b37ceeed7d851b3dd420887a
wangzhiwei <vancer@pku.edu.cn>
2018-10-14 22:33:47 +0800

    3

--- app/src/main/java/cn/edu/pku/vancer/hiweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/vancer/hiweather/MainActivity.java
@@ -41,124 +41,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
             Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
         }
     }
-}
-    private void parseXML(String xmldata) {
-            int fengxiangCount =0;
-            int fengliCount =0;
-            int dateCount =0;
-            int highCount =0;
-            int lowCount =0;
-            int typeCount =0;
-        try {
-            XmlPullParserFactory fac = XmlPullParserFactory.newInstance();
-            XmlPullParser xmlPullParser = fac.newPullParser();
-            xmlPullParser.setInput(new StringReader(xmldata));
-            int eventType = xmlPullParser.getEventType();
-            Log.d("myWeather", "parseXML");
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                switch (eventType) {
-                    //判断当前事件是否为文档开始事件
-                    case XmlPullParser.START_DOCUMENT:
-                        break;
-                    //判断当前事件是否为标签元素开始事件
-                    case XmlPullParser.START_TAG:
-                        if (xmlPullParser.getName().equals("city")) {
-                            eventType = xmlPullParser.next();
-                            Log.d("myWeather", "city:    " +xmlPullParser.getText());
-
-                        } else if (xmlPullParser.getName().equals("updatetime")) {
-                            eventType = xmlPullParser.next();
-                            Log.d("myWeaher", "updatetime:   " + xmlPullParser.getText());
-                        } else if (xmlPullParser.getName().equals("shidu")) {
-                            eventType = xmlPullParser.next();
-                            Log.d("myWeather", "shidu:   " + xmlPullParser.getText());
-                        } else if (xmlPullParser.getName().equals("wendu")) {
-                            eventType = xmlPullParser.next();
-                            Log.d("myWeather","wendu:    "+xmlPullParser.getText());
-                        } else if (xmlPullParser.getName().equals("pm25")) {
-                            eventType = xmlPullParser.next();
-                            Log.d("myWeather","pm25:    "+xmlPullParser.getText());
-                        } else if (xmlPullParser.getName().equals("quality")) {
-                            eventType = xmlPullParser.next();
-                            Log.d("myWeather","quality:    "+xmlPullParser.getText());
-                        } else if (xmlPullParser.getName().equals("fengxiang") && fengxiangCount==0) {
-                            eventType = xmlPullParser.next();
-                            Log.d("myWeather","fengxiang:    "+xmlPullParser.getText());
-                            fengxiangCount++;
-                        } else if (xmlPullParser.getName().equals("fengli") && fengliCount ==0) {
-                            eventType = xmlPullParser.next();
-                            Log.d("myWeather", "fengli:   " + xmlPullParser.getText());
-                            fengliCount++;
-                        } else if (xmlPullParser.getName().equals("date") && dateCount == 0) {
-                            eventType = xmlPullParser.next();
-                            Log.d("myWeather", "date:     " + xmlPullParser.getText());
-                            dateCount++;
-                        } else if (xmlPullParser.getName().equals("high") && highCount ==0) {
-                            eventType = xmlPullParser.next();
-                            Log.d("myWeather", "high:   " + xmlPullParser.getText());
-                            highCount++;
-                        } else if (xmlPullParser.getName().equals("low") && lowCount ==0) {
-                            eventType = xmlPullParser.next();
-                            Log.d("myWeather", "low:   " + xmlPullParser.getText());
-                            lowCount++;
-                        } else if (xmlPullParser.getName().equals("type") && typeCount ==0) {
-                            eventType = xmlPullParser.next();
-                            Log.d("myWeather", "type:   " + xmlPullParser.getText());
-                            typeCount++;
-
-                            break;
-                        }
-                    //判断当前事件是否为标签元素结束事件
-                    case XmlPullParser.END_TAG:
-                        break;
-                }
-                //进入下一个元素并触发相应事件
-                eventType = xmlPullParser.next();
-            }
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-    private void queryWeatherCode(String cityCode) {
-    final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey="+cityCode;
-    Log.d("myweather",address);
-    new Thread(new Runnable() {
-        @Override
-        public void run() {
-            HttpURLConnection con=null;
-            try{
-                URL url = new URL(address);
-                con = (HttpURLConnection)url.openConnection();
-                con.setRequestMethod("GET");
-                con.setConnectTimeout(8000);
-                con.setReadTimeout(8000);
-                InputStream in = con.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                StringBuilder response = new StringBuilder();
-                String str;
-                while((str=reader.readLine())!=null){
-                    response.append(str);
-                    Log.d("myWeather",str);
-                }
-                String responseStr=response.toString();
-                Log.d("myWeather",responseStr);
-                parseXML(responseStr);
-            }catch (Exception e){
-                e.printStackTrace();
-            }finally {
-                if(con != null){
-                    con.disconnect();
-                }
-            }
-        }
-    }).start();
-}
 
     @Override
-    public void onClick(View view) {
+    public void onClick (View view){
         if (view.getId() == R.id.title_update_btn) {
 
             SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
@@ -175,3 +60,118 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }
     }
 
+    private void queryWeatherCode (String cityCode){
+        final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + cityCode;
+        Log.d("myweather", address);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection con = null;
+                try {
+                    URL url = new URL(address);
+                    con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setConnectTimeout(8000);
+                    con.setReadTimeout(8000);
+                    InputStream in = con.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String str;
+                    while ((str = reader.readLine()) != null) {
+                        response.append(str);
+                        Log.d("myWeather", str);
+                    }
+                    String responseStr = response.toString();
+                    Log.d("myWeather", responseStr);
+                    parseXML(responseStr);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (con != null) {
+                        con.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void parseXML(String xmldata) {
+        int fengxiangCount = 0;
+        int fengliCount = 0;
+        int dateCount = 0;
+        int highCount = 0;
+        int lowCount = 0;
+        int typeCount = 0;
+        try {
+            XmlPullParserFactory fac = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = fac.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmldata));
+            int eventType = xmlPullParser.getEventType();
+            Log.d("myWeather", "parseXML");
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    //判断当前事件是否为文档开始事件
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+                    //判断当前事件是否为标签元素开始事件
+                    case XmlPullParser.START_TAG:
+                        if (xmlPullParser.getName().equals("city")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "city:    " + xmlPullParser.getText());
+
+                        } else if (xmlPullParser.getName().equals("updatetime")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeaher", "updatetime:   " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("shidu")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "shidu:   " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("wendu")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "wendu:    " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("pm25")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "pm25:    " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("quality")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "quality:    " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("fengxiang") && fengxiangCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "fengxiang:    " + xmlPullParser.getText());
+                            fengxiangCount++;
+                        } else if (xmlPullParser.getName().equals("fengli") && fengliCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "fengli:   " + xmlPullParser.getText());
+                            fengliCount++;
+                        } else if (xmlPullParser.getName().equals("date") && dateCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "date:     " + xmlPullParser.getText());
+                            dateCount++;
+                        } else if (xmlPullParser.getName().equals("high") && highCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "high:   " + xmlPullParser.getText());
+                            highCount++;
+                        } else if (xmlPullParser.getName().equals("low") && lowCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "low:   " + xmlPullParser.getText());
+                            lowCount++;
+                        } else if (xmlPullParser.getName().equals("type") && typeCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "type:   " + xmlPullParser.getText());
+                            typeCount++;
+
+                            break;
+                        }
+                        //判断当前事件是否为标签元素结束事件
+                    case XmlPullParser.END_TAG:
+                        break;
+                }
+                //进入下一个元素并触发相应事件
+                eventType = xmlPullParser.next();
+            }
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
